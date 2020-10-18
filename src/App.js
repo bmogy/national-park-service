f0a573993d879492bffa45c2a7e927eab25392b6
bmogy <bmogy@outlook.com>
2020-10-18 12:43:43 -0500

    I made another fix for the links problem i have

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import Home from "./park-app/body/home"
 import Events from "./park-app/body/Events/Events"
 import Parks from "./park-app/body/Parks/Parks"
 import './App.css';
-import { BrowserRouter, Route, HashRouter } from "react-router-dom"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <HashRouter>
-          <Route path="/" component={Home} exact={true}></Route>
-          <Route path="/events" component={Events} exact={true}></Route>
-          <Route path="/parks" component={Parks}></Route>
-        </HashRouter>
+        <Switch>
+          <Route path="gh-pages-url/" component={Home} exact={true}></Route>
+          <Route path="/gh-pages-url/events" component={Events} exact={true}></Route>
+          <Route path="gh-pages-url/parks" component={Parks}></Route>
+        </Switch>
       </BrowserRouter>
     </div>
   );
