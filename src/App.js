23117cc1810402437ba51f1bde2dc025bbb3b5c6
bmogy <bmogy@outlook.com>
2020-10-18 13:13:15 -0500

    I added a hashrouter this time

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import Home from "./park-app/body/home"
 import Events from "./park-app/body/Events/Events"
 import Parks from "./park-app/body/Parks/Parks"
 import './App.css';
-import { BrowserRouter, Route, Switch } from "react-router-dom"
+import { BrowserRouter, Route, Switch, HashRouter } from "react-router-dom"
 
 
 const App = () => {
   return (
     <div className="App">
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route path="/" component={Home} exact={true}></Route>
           <Route path="/events" component={Events} exact={true}></Route>
           <Route path="/parks" component={Parks}></Route>
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
